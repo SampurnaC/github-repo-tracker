1a2bfe726a40be1cceb96c9ac5ea18a52b92ec0b
sam <sampurna.chapagainnn@gmail.com>
2022-11-12 20:03:23 +0000

    style slider for displaying repos lists

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,11 +1,36 @@
+import React from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 import SingleRepo from '../SingleRepo/SingleRepo';
+import './Slider.css';
 
 const Slider = ({ repos }) => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
   return (
     <div className="box">
-      {repos.map((repo) => (
-        <SingleRepo key={repo.id} repo={repo} />
-      ))}
+      <Carousel responsive={responsive} infinite={true}>
+        {repos?.map((repo) => (
+          <SingleRepo key={repo.id} repo={repo} />
+        ))}
+      </Carousel>
     </div>
   );
 };
