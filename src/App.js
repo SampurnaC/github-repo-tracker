f44f34efdd5968c5d1ed8afbfc4df4cea72f48ff
sam <sampurna.chapagainnn@gmail.com>
2022-11-12 18:14:58 +0000

    install react-router and give pathname to home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from 'react';
+import { Route, Routes } from 'react-router';
 import './App.css';
 import * as Pages from './pages';
 
 function App() {
   return (
     <div className="App">
-      <Pages.Home />
+      <Routes>
+        <Route path="/" element={<Pages.Home />} />
+        {/* <Route path="/:name" element={<Pages.Home />} /> */}
+      </Routes>
     </div>
   );
 }
--- src/components/SingleRepo/SingleRepo.js
+++ src/components/SingleRepo/SingleRepo.js
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom';
+
 const SingleRepo = ({ repo }) => {
   return (
     <div className="repo">
       <h2>{repo.name}</h2>
+      <Link to="#"></Link>
     </div>
   );
 };
